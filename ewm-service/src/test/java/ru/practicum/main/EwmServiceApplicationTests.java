95d72b3682ea7dcd7618956b2705003d39342a4d
Sultan Abdukarimov <abdukarimov.05@gmail.com>
2025-11-09 20:42:04 +0500

    stats

---
+++ ewm-service/src/test/java/ru/practicum/main/EwmServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.practicum.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EwmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
